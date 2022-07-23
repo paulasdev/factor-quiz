4bd9b2e1389f4ad26f1445e756150483dc9312c1
Paula da Silva <carolpaulag@gmail.com>
2022-07-23 20:26:08 +0000

    Add event listener for categories button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // https://opentdb.com/api.php?amount=10
 
-const _question = document.getElementById('question');
-const _options = document.querySelectorAll('.quiz-options');
+const questionRef = document.Selector('#question');
+const optionsRef = document.querySelectorAll('.quiz-options');
+const btnCategoryRef = Array.from(document.querySelectorAll(".btn-categories"))
 
 async function loadQuestion() {
     const APIUrl = 'https://opentdb.com/api.php?amount=1';
@@ -17,7 +18,13 @@ function showQuestion(data){
     let optionsList = incorrectAnswer;
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
  
-    _question.innerHTML = `${data.question} <br> <span class = "category"> ${data.category} </span>`;
+    questionRef.innerHTML = `${data.question} <br> <span class = "category"> ${data.category} </span>`;
 }
 
 loadQuestion();
+
+btnCategoryRef.forEach(btn => {
+    btn.addEventListener("click", (event)=> {
+        
+    })
+})
