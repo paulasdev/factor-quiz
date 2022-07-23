6b1be8a8d9ca0838f832b9efacb730b2cb96a902
Paula da Silva <carolpaulag@gmail.com>
2022-07-23 21:00:08 +0000

    Add function show question and event listener by categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 // https://opentdb.com/api.php?amount=10
 
-const questionRef = document.Selector('#question');
+const questionRef = document.querySelector('#question');
 const optionsRef = document.querySelectorAll('.quiz-options');
 const btnCategoryRef = Array.from(document.querySelectorAll(".btn-categories"))
 
-async function loadQuestion() {
-    const APIUrl = 'https://opentdb.com/api.php?amount=1';
+async function loadQuestion(categoryID) {
+    const APIUrl = `https://opentdb.com/api.php?amount=1&category=${categoryID}`;
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
     // console.log(data.results[0]);
@@ -13,6 +13,10 @@ async function loadQuestion() {
 }
 
 function showQuestion(data){
+    const question = {
+        correctAnswer: data.correct_answer,
+        incorrectAnswer: data.incorrectAnswer,
+    }
     let correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
@@ -21,10 +25,26 @@ function showQuestion(data){
     questionRef.innerHTML = `${data.question} <br> <span class = "category"> ${data.category} </span>`;
 }
 
-loadQuestion();
+// loadQuestion();
 
 btnCategoryRef.forEach(btn => {
-    btn.addEventListener("click", (event)=> {
-        
+    btn.addEventListener("click", (event) => {
+        switch (event.target.id) {
+            case "films":
+                loadQuestion(11)
+                break;
+            case "animals":
+                loadQuestion(27)
+                break;
+            case "cars":
+                loadQuestion(28)
+                break;
+            case "sports":
+                loadQuestion(21)
+                break;
+
+            default:
+                break;
+        }
     })
 })
