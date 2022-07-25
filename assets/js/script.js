fa4584878cec8ecb862780054cfeffb6c7c6d491
Paula da Silva <carolpaulag@gmail.com>
2022-07-25 16:08:38 +0000

    Add check answers function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 // https://opentdb.com/api.php?amount=10
 
 const questionRef = document.querySelector('#question');
-// const optionsRef = document.querySelectorAll('.quiz-options');
 const optionsRef = document.querySelector('#options');
 const btnCategoryRef = Array.from(document.querySelectorAll(".btn-categories"))
+const correctScoreRef = document.getElementById('correct-score');
+const totalQuestionRef = document.getElementById('total-question');
+const checkRef = document.getElementById('check-answer');
+const resultref = document.getElementById('result');
+const playAgainRef = document.getElementById('play-again');
+
+let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
+
+function addEventListener(){
+    optionsRef.addEventListener('click', checkAnswer);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // loadQuestion();
+    totalQuestionRef.textContent = totalQuestion;
+    correctScoreRef.textContent = correctScore;
+});
 
 async function loadQuestion(categoryID) {
     const APIUrl = `https://opentdb.com/api.php?amount=1&category=${categoryID}`;
@@ -30,6 +46,33 @@ function showQuestion(data){
             <li> ${index + 1}. <span> ${option} </span> </li>
         `).join('')}
      `;
+     selectOption()
+}
+
+//Function to select answer
+function selectOption() {
+    optionsRef.querySelectorAll('li').forEach((option) => {
+        option.addEventListener('click', () => {
+            if(optionsRef.querySelector('.selected')) {
+                const activeOption = optionsRef.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        })
+    })
+}
+
+function checkAnswer() {
+    if(optionsRef.querySelector('.selected')) {
+        let selectedAnswer = optionsRef.querySelector('.selected span').
+        textContent;
+        if(selectedAnswer == correctAnswer) {
+            correctScore++;
+            resultref.innerhtml = `<p> <i class = "fas fa-check"></i>Correct Answer!</p>`;
+        } else {
+            resultref.innerHTML = `<p> <i class = "fas fa-check"></i>Incorrect Answer! <small><b>Correct Answer: </b> ${correctAnswer}</small></p>`;
+        }
+    }
 }
 
 //Function to select the question by categories
@@ -55,3 +98,4 @@ btnCategoryRef.forEach(btn => {
         }
     })
 })
+
