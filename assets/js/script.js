9429cf552a1a33316da4facedef7fbe2526568b0
Paula da Silva <carolpaulag@gmail.com>
2022-07-28 11:51:42 +0000

    Add function to select the answers and convert html/string

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,22 @@ const optionsRef = document.querySelector('#options');
 const btnCategoryRef = Array.from(document.querySelectorAll(".btn-categories"))
 const correctScoreRef = document.getElementById('correct-score');
 const totalQuestionRef = document.getElementById('total-question');
-const checkRef = document.getElementById('check-answer');
-const resultref = document.getElementById('result');
+const checkAnswerRef = document.getElementById('check-answer');
 const playAgainRef = document.getElementById('play-again');
+const resultsref = document.getElementById('result');
 
 let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
 
-function addEventListener(){
-    optionsRef.addEventListener('click', checkAnswer);
+function eventListeners() {
+    checkAnswerRef.addEventListener('click', checkAnswer);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // loadQuestion();
+    loadQuestion();
+    eventListeners();
     totalQuestionRef.textContent = totalQuestion;
     correctScoreRef.textContent = correctScore;
+
 });
 
 async function loadQuestion(categoryID) {
@@ -28,28 +30,56 @@ async function loadQuestion(categoryID) {
     showQuestion(data.results[0]);
 }
 
-//Function to display the questions
+//Function to display the questions and answers
 
 function showQuestion(data){
-    const question = {
-        correctAnswer: data.correct_answer,
-        incorrectAnswer: data.incorrectAnswer,
-    }
-    let correctAnswer = data.correct_answer;
+    correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, correctAnswer);
- 
+  
+
     questionRef.innerHTML = `${data.question} <br> <span class = "category"> ${data.category} </span>`;
     optionsRef.innerHTML = `${optionsList.map((option, index) => `
-            <li> ${index + 1}. <span> ${option} </span> </li>
-        `).join('')}
-     `;
-     selectOption()
+             <li> ${index + 1}. <span> ${option} </span> </li>
+         `).join('')}
+      `;
+      selectOption()
+}
+//Function to select answer
+function selectOption() {
+    optionsRef.querySelectorAll('li').forEach((option) => {
+        option.addEventListener('click', () => {
+          if(optionsRef.querySelector('.selected')) {
+            const activeOption = optionsRef.querySelector('.selected');
+            activeOption.classList.remove('selected');
+          }
+          option.classList.add('selected');
+        })
+    })
+console.log(correctAnswer);
 }
 
+//check answer
+function checkAnswer() {
+    checkAnswerRef.disbled = true;
+    if(optionsRef.querySelector('.selected')) {
+        let selectedAnswer = optionsRef.querySelector('.selected span').
+        textContent;
+        if(selectedAnswer == HTMLDecode(correctAnswer)) {
+            correctScore++;
+            resultsref.innerHTML = `<p> <i class = "fas fa-check">Correct Answer!</i> </p>`;
+        } else {
+            resultsref.innerHTML = `<p> <i class = "fas fa-times">Incorrect Answer!</p> <p><small><b>Correct Answer: </b>${correctAnswer}</small></i></p>`;
+        }
+    }
+}
 
-//Function to select answer
+//convert html entities into normal text of correct answer id there is any
+function HTMLDecode(textString) {
+    let doc = new DOMParser().parseFromString(textString, "text/html");
+    return doc.documentElement.textContent;
+}
 
 //Function to select the question by categories
 
